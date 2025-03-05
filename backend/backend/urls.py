3112281e0b01f15836962d66f57dfe1111f50b75
Waleed Lugod <lugodwaleed@gmail.com>
2025-03-05 17:41:17 +0800

    TC-8 create app boilerplate

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('search/', include("podcast_search.urls"))
 ]
