decf7a2cc3536f3f3d3302bad75f6a28a6ee6bdf
Waleed Lugod <lugodwaleed@gmail.com>
2025-03-07 08:17:21 +0800

    TC-8 include all fields in api

--- backend/podcast_search/serializers.py
+++ backend/podcast_search/serializers.py
@@ -5,4 +5,4 @@ from .models import Podcast
 class PodcastSerializer(serializers.ModelSerializer):
     class Meta:
         model = Podcast
-        fields = ["id", "title"]
+        fields = ["id", "title", "transcript", "description", "is_featured", "audio", "video"]
