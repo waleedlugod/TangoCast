1932cd0baa3b77751f4b1f4d3104b31818d6a3cb
Chesca <fdjr401@gmail.com>
2025-03-12 22:20:19 +0800

    Add sleep timer functionality to podcast player

--- frontend/src/Player.jsx
+++ frontend/src/Player.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import './Player.css'
@@ -16,24 +17,40 @@ const podcastShowName = "The Magnus Archives"
 const podcastEpisodeName = "Anglerfish"
 const podcastEpisodeNumber = 1;
 
-function toggleTimerOptions(e) {
-  e.preventDefault();
-  const timerOptions = document.querySelector("#js-player-timer-options");
-  timerOptions.classList.toggle("player-timer-options-shown");
-}
-
 export default function Player() {
-  
-  const audioUrl = "http://localhost:8000/search/";
-  const audio = fetch(audioUrl).then((res) => {
-    return res.json();
-  });
 
-  console.log(audio);
+  const audioRef = useRef(null);
+
+  // const audioUrl = "http://localhost:8000/search/";
+  // const audio = fetch(audioUrl).then((res) => {
+  //   return res.json();
+  // });
+  //
+  // console.log(audio);
+
+  function toggleTimerOptions(e) {
+    e.preventDefault();
+    let timerOptions = document.querySelector("#js-player-timer-options");
+    timerOptions.classList.toggle("player-timer-options-shown");
+  }
+  
+  // TODO: add functionality for saving duration even when audio isn't playing
+  // TODO: add dot under timer icon if sleep timer is set
+  function startSleepTimer(duration) {
+    let audio = audioRef.current.audio.current;
+    let timerOptions = document.querySelector("#js-player-timer-options");
+    if (!audio.paused) {
+      timerOptions.classList.toggle("player-timer-options-shown");
+      setTimeout(() => {
+        audio.pause();
+      }, duration);
+    }
+}
 
   return(
     <>
       <div className="player-container">
+        {/* put into its own component */}
         <div className="player-info">
           <img src={podcastImage} />
           <div className="player-info-text">
@@ -48,13 +65,15 @@ export default function Player() {
           layout="stacked-reverse"
           customAdditionalControls={playerControls}
           progressJumpSteps={playerJumpSteps}
+          ref={audioRef}
         />
+        {/* put into its own component */}
         <div className="player-additional-controls">
           <div className="player-timer-container">
             <button onClick={toggleTimerOptions}><img src={timerIcon} /></button>
             <div className="player-timer-options" id="js-player-timer-options">
-              <button>2 sec</button>
-              <button>5 sec</button>
+              <button onClick={() => {startSleepTimer(60000);}}>1 min</button>
+              <button onClick={() => {startSleepTimer(120000);}}>2 min</button>
             </div>
           </div>
           <button><img src={videoIcon} /></button>
