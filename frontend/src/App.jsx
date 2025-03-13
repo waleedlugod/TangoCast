b761d0099c83dcb72176efcb4ec13a7b308da503
Waleed Lugod <lugodwaleed@gmail.com>
2025-03-13 21:45:19 +0800

    add routing and install react query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import './App.css'
-import Player from './Player.jsx'
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Player from "./Player.jsx";
+import Search from "./Search.jsx";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-      <div className="main-container">
-        <p>This is the main app container.</p>
-      </div>
-      <Player />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Player />} />
+          <Route path="search" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
