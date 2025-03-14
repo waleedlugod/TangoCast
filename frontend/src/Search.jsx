1330076c78e922b247acbd1f1e5aafe277399deb
Waleed Lugod <lugodwaleed@gmail.com>
2025-03-14 17:03:41 +0800

    remove unused function

--- frontend/src/Search.jsx
+++ frontend/src/Search.jsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 async function fetchPodcasts(search) {
