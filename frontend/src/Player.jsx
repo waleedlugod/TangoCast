b64ef8a66b52a54b0932d1caf5378bab1c6a1c48
Chesca <fdjr401@gmail.com>
2025-03-20 23:28:26 +0800

    Fix functionality of sleep timer and add a speed indicator to the podcast player

--- frontend/src/Player.jsx
+++ frontend/src/Player.jsx
@@ -7,15 +7,28 @@ import videoIcon from '/video.svg';
 import transcriptIcon from '/transcript.svg';
 import circleIcon from '/icon-circle.svg';
 
-export default function Player({ podcast, handleVideo, handleTranscript }) {
+function PlayerPodcast(props) {
+  const podcast = props.podcast;
+  return (
+    <div className="player__info">
+      <img src={podcast.image} alt={podcast.imageAlt}/>
+      <div className="player__info-text">
+        <p className="player__info-show">{podcast.showName}</p>
+        <p className="player__info-episode">Episode {podcast.epNumber}: {podcast.epName}</p>
+      </div>
+    </div>
+  );
+}
 
-  const audioRef = useRef(null);
-  const playerControls = [];
-  const playerJumpSteps = {
-    backward: 5000,
-    forward: 5000,
-  };
+function PlayerSpeed() {
+  return (
+    <div>
+      Speed: <span>x1</span>
+    </div>
+  )
+}
 
+function PlayerAdditional({ handleVideo, handleTranscript, startTimer, pauseTimer, stopTimer }) {
   function toggleVideo() {
     let videoCircle = document.querySelector("#js-video-circle");
     videoCircle.classList.toggle("visible");
@@ -25,42 +38,119 @@ export default function Player({ podcast, handleVideo, handleTranscript }) {
     let transcriptCircle = document.querySelector("#js-transcript-circle");
     transcriptCircle.classList.toggle("visible");
   }
-
-  function toggleTimerOptions(e) {
-    e.preventDefault();
-    let audio = audioRef.current.audio.current;
-    let timerOptions = document.querySelector("#js-player-timer-options");
-    if (!audio.paused) {
-      timerOptions.classList.toggle("player__additional-timer-options--shown");
-    }
-  }
   
-  // TODO: add functionality for cancelling sleep timer if player is paused
-  function startSleepTimer(duration) {
+  function toggleTimerOptions() {
+    let timerOptions = document.querySelector("#js-player-timer-options"); 
+    timerOptions.classList.toggle("visible");
+  }
+
+  return (
+    <div className="player__additional">
+      <div className="player__additional-timer">
+        <button className="player__additional-timer-button" onClick={toggleTimerOptions}>
+          <img className="player__additional-icon" src={timerIcon} alt="Sleep timer" />
+          <img className="player__additional-circle" id="js-timer-circle" src={circleIcon} alt="Circle" />
+        </button>
+        <div className="player__additional-timer-options" id="js-player-timer-options">
+          <button onClick={() => {startTimer(60000);}}>1 min</button>
+          <button onClick={() => {startTimer(120000);}}>2 min</button>
+          <button onClick={() => {stopTimer();}} className="player__additional-timer-stop" id="js-player-timer-stop">Stop Timer</button>
+        </div>
+      </div>
+      <button className="player__additional-video-button" onClick={() => {handleVideo(); toggleVideo();}}>
+        <img className="player__additional-icon" src={videoIcon} alt="Enable or disable video" />
+        <img className="player__additional-circle" id="js-video-circle" src={circleIcon} alt="Circle" />
+      </button>
+      <button className="player__additional-transcript-button" onClick={() => {handleTranscript(); toggleTranscript();}}>
+        <img className="player__additional-icon" src={transcriptIcon} alt="Enable or disable transcript" />
+        <img className="player__additional-circle" id="js-transcript-circle" src={circleIcon} alt="Circle" />
+      </button>
+    </div>
+  );
+}
+
+export default function Player({ podcast, handleVideo, handleTranscript }) {
+
+  const audioRef = useRef(null);
+  const timerIDRef = useRef(null);
+  const timerRemainingRef = useRef(0);
+  const playerControls = [<PlayerSpeed key="0" />];
+  const playerJumpSteps = {
+    backward: 5000,
+    forward: 5000,
+  };
+  let sleepTimer;
+
+  // only one timer can exist at a time
+  const Timer = function(callback, delay) {
+    let start;
+    timerRemainingRef.current = delay;
+
+    this.pause = function() {
+      clearTimeout(timerIDRef.current);
+      timerIDRef.current = null;
+      timerRemainingRef.current = timerRemainingRef.current - (Date.now() - start);
+    }
+
+    this.resume = function() {
+      if (timerIDRef.current) {
+        return;
+      }
+      start = Date.now();
+      timerIDRef.current = setTimeout(callback, timerRemainingRef.current);
+    }
+
+    this.resume();
+  }
+
+  function startTimer(duration) {
     let audio = audioRef.current.audio.current;
     let timerOptions = document.querySelector("#js-player-timer-options");
     let timerCircle = document.querySelector("#js-timer-circle");
-    if (!audio.paused) {
-      timerOptions.classList.toggle("player__additional-timer-options--shown");
-      timerCircle.classList.toggle("visible");
-      setTimeout(() => {
-        audio.pause();
-      }, duration);
+    let timerStop = document.querySelector("#js-player-timer-stop");
+    timerOptions.classList.toggle("visible");
+    timerCircle.classList.toggle("visible");
+    timerStop.classList.toggle("visible");
+    sleepTimer = new Timer(() => {audio.pause(); stopTimer();}, duration);
+    if (audio.paused) {
+      pauseTimer();
+    }
+  }
+
+  function resumeTimer() {
+    sleepTimer.resume();
+  }
+
+  function pauseTimer() {
+    sleepTimer.pause();
+  }
+
+  function stopTimer() {
+    sleepTimer.pause();
+    sleepTimer = null;
+    let timerCircle = document.querySelector("#js-timer-circle");
+    let timerStop = document.querySelector("#js-player-timer-stop");
+    timerCircle.classList.toggle("visible");
+    timerStop.classList.toggle("visible");
+  }
+
+  function handlePlay() {
+    if (!timerIDRef.current && sleepTimer) {
+      resumeTimer();
+    }
+  }
+
+  function handlePause() {
+    if (timerIDRef.current && sleepTimer) {
+      pauseTimer();
     }
   }
 
   return(
     <>
       <div className="player">
-        {/* put into its own component */}
-        <div className="player__info">
-          <img src={podcast.image} alt={podcast.imageAlt}/>
-          <div className="player__info-text">
-            <p className="player__info-show">{podcast.showName}</p>
-            <p className="player__info-episode">Episode {podcast.epNumber}: {podcast.epName}</p>
-          </div>
-        </div>
-        <AudioPlayer 
+        <PlayerPodcast podcast={podcast} />
+        <AudioPlayer
           src={podcast.audio} 
           className="player__controls"
           showSkipControls="true"
@@ -68,28 +158,10 @@ export default function Player({ podcast, handleVideo, handleTranscript }) {
           customAdditionalControls={playerControls}
           progressJumpSteps={playerJumpSteps}
           ref={audioRef}
+          onPlay={handlePlay}
+          onPuase={handlePause}
         />
-        {/* put into its own component */}
-        <div className="player__additional">
-          <div className="player__additional-timer">
-            <button className="player__additional-timer-button" onClick={toggleTimerOptions}>
-              <img className="player__additional-icon" src={timerIcon} alt="Sleep timer" />
-              <img className="player__additional-circle" id="js-timer-circle" src={circleIcon} alt="Circle" />
-            </button>
-            <div className="player__additional-timer-options" id="js-player-timer-options">
-              <button onClick={() => {startSleepTimer(60000);}}>1 min</button>
-              <button onClick={() => {startSleepTimer(120000);}}>2 min</button>
-            </div>
-          </div>
-          <button className="player__additional-video-button" onClick={() => {handleVideo(); toggleVideo();}}>
-            <img className="player__additional-icon" src={videoIcon} alt="Enable or disable video" />
-            <img className="player__additional-circle" id="js-video-circle" src={circleIcon} alt="Circle" />
-          </button>
-          <button className="player__additional-transcript-button" onClick={() => {handleTranscript(); toggleTranscript();}}>
-            <img className="player__additional-icon" src={transcriptIcon} alt="Enable or disable transcript" />
-            <img className="player__additional-circle" id="js-transcript-circle" src={circleIcon} alt="Circle" />
-          </button>
-        </div>
+        <PlayerAdditional handleVideo={handleVideo} handleTranscript={handleTranscript} startTimer={startTimer} pauseTimer={pauseTimer} stopTimer={stopTimer}/>
       </div>
     </>
   );
