a40059c2dde639ac16ef0a78d203e1188436a458
Waleed Lugod <lugodwaleed@gmail.com>
2025-03-21 17:40:33 +0800

    create test for podcast text

--- backend/podcast_search/tests.py
+++ backend/podcast_search/tests.py
@@ -2,6 +2,19 @@ from django.test import TestCase
 from .models import Podcast
 
 # Create your tests here.
-class TextsTestCase(TestCase):
-    def test_thing(self):
-        self.assertEqual(1, 1)
+class ChuckleSandwichTest(TestCase):
+    def setUp(self):
+        Podcast.objects.create(
+            title="Chuckle Sandwich",
+            transcript="some transcript",
+            description="Chuckle Sandwich was a comedy podcast hosted by Ted Nivison, jschlatt, Slimecicle, and Tucker Keane",
+            is_featured=True,
+            category="comedy"
+            )
+
+    def test_texts(self):
+        podcast = Podcast.objects.get(title="Chuckle Sandwich")
+        self.assertEqual(podcast.transcript, "some transcript")
+        self.assertEqual(podcast.description, "Chuckle Sandwich was a comedy podcast hosted by Ted Nivison, jschlatt, Slimecicle, and Tucker Keane")
+        self.assertEqual(podcast.category, "comedy")
+        self.assertEqual(podcast.is_featured, True)
