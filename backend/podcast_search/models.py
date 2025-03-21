e16d38eb0a6de12a50e396562e3978459f92f988
Waleed Lugod <lugodwaleed@gmail.com>
2025-03-21 18:55:47 +0800

    create podcast test

--- backend/podcast_search/models.py
+++ backend/podcast_search/models.py
@@ -7,7 +7,7 @@ class Podcast(models.Model):
     transcript = models.TextField(blank=True, default="")
     description = models.TextField(blank=True, default="")
     is_featured = models.BooleanField(default=False)
-    audio = models.FileField(upload_to='audio/', null=True, blank=True)
+    audio = models.FileField(upload_to='audios/', null=True, blank=True)
     video = models.FileField(upload_to='videos/', null=True, blank=True)
     thumbnail = models.FileField(upload_to='photos/', null=True, blank=True)
     category = models.CharField(max_length=255, verbose_name="category", blank=True)
--- backend/podcast_search/tests.py
+++ backend/podcast_search/tests.py
@@ -1,13 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 from .models import Podcast
+import shutil
+
+TEST_DIR = "test_data"
 
 # Create your tests here.
 class ChuckleSandwichTest(TestCase):
+    @override_settings(MEDIA_ROOT=(TEST_DIR + "/media"))
     def setUp(self):
         Podcast.objects.create(
             title="Chuckle Sandwich",
             transcript="some transcript",
             description="Chuckle Sandwich was a comedy podcast hosted by Ted Nivison, jschlatt, Slimecicle, and Tucker Keane",
+            audio = SimpleUploadedFile(name="Episode_1.mp3", content=b"this is some audio", content_type="audio/mpeg"),
+            video = SimpleUploadedFile(name="Episode_1.mp4", content=b"this is some video", content_type="video/mp4"),
+            thumbnail = SimpleUploadedFile(name="Episode_1.png", content=b"this is some thumbnail", content_type="image/png"),
             is_featured=True,
             category="comedy"
             )
@@ -18,3 +26,10 @@ class ChuckleSandwichTest(TestCase):
         self.assertEqual(podcast.description, "Chuckle Sandwich was a comedy podcast hosted by Ted Nivison, jschlatt, Slimecicle, and Tucker Keane")
         self.assertEqual(podcast.category, "comedy")
         self.assertEqual(podcast.is_featured, True)
+        self.assertEqual(podcast.audio.name, "audios/Episode_1.mp3")
+        self.assertEqual(podcast.video.name, "videos/Episode_1.mp4")
+        self.assertEqual(podcast.thumbnail.name, "photos/Episode_1.png")
+        self.assertTrue(podcast.is_featured)
+
+    def tearDown(self):
+        shutil.rmtree(TEST_DIR, ignore_errors=True)
