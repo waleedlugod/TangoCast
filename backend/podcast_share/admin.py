c570f06a397c275a52a0304dcfef0ec2d586432f
Waleed Lugod <lugodwaleed@gmail.com>
2025-04-06 22:17:43 +0800

    adjust admin view

--- backend/podcast_share/admin.py
+++ backend/podcast_share/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from .models import SharedPodcast
 
+
 class SharedPodcastAdmin(admin.ModelAdmin):
-    list_display = ('podcast', 
-                    'shared_by', 
-                    'shared_at', 
-                    'get_share_link') 
+    list_display = ("podcast", "shared_by", "shared_at", "get_share_link")
+    readonly_fields = ("get_share_link",)
 
     def get_share_link(self, obj):
         return obj.get_share_link()
-    
-    get_share_link.short_description = "Share Link" 
+
+    get_share_link.short_description = "Share Link"
+
 
 admin.site.register(SharedPodcast, SharedPodcastAdmin)
--- backend/podcast_share/models.py
+++ backend/podcast_share/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from podcast_search.models import Podcast
-from django.conf import settings
+from user_management.models import UserModel
 
 
 class SharedPodcast(models.Model):
     podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
-    shared_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    shared_by = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     shared_at = models.DateTimeField(auto_now_add=True)
 
-    # def get_share_link(self):
-    #     return f"http://127.0.0.1:8000/podcast/{self.podcast.share_uuid}"
+    def get_share_link(self):
+        return f"http://127.0.0.1:8000/podcast/{self.podcast.id}"
 
     def __str__(self):
         return f"{self.podcast.title} shared by {self.shared_by.username}"
