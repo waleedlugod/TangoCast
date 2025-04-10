47fa056684d59e46c7a274eef94fc8de06125a8f
Arthur Lluisma <ajclluisma@gmail.com>
2025-04-10 23:44:45 +0800

    Make migrations for Creator and Listener

--- backend/user_management/models.py
+++ backend/user_management/models.py
@@ -11,15 +11,14 @@ class UserModel(AbstractUser):
         ("administrator", "Admin"),
     )
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, default="listenerUser")
-    user_id = models.BigAutoField(primary_key=True)
     profile_photo = models.ImageField(upload_to="photos/", null=True, blank=True)
     banner_photo = models.ImageField(upload_to="photos/", null=True, blank=True)
     bio = models.TextField(blank=True)
-    instagram_social = models.URLField()
-    x_social = models.URLField()
+    instagram_social = models.URLField(null=True, blank=True)
+    x_social = models.URLField(null=True, blank=True)
 
 
-class CreatorModel(AbstractUser):
+class CreatorModel(models.Model):
     creator_id = models.ForeignKey(
         UserModel,
         null=True,
@@ -30,10 +29,10 @@ class CreatorModel(AbstractUser):
     staff = models.TextField()
 
 
-class ListenerModel(AbstractUser):
+class ListenerModel(models.Model):
     listener_id = models.ForeignKey(
         UserModel, on_delete=models.CASCADE, related_name="listener"
     )
     follows = models.ForeignKey(
-        CreatorModel, on_delete=models.SET_NULL, related_name="creator"
+        CreatorModel, null=True, blank=True, on_delete=models.SET_NULL, related_name="creator"
     )
