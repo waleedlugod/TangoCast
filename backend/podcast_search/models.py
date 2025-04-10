5a461b1ab79da76eb26aa137c33cacaff99db060
Arthur Lluisma <ajclluisma@gmail.com>
2025-04-11 02:08:10 +0800

    Add needed attributes to Podcast model

--- backend/podcast_search/models.py
+++ backend/podcast_search/models.py
@@ -11,6 +11,8 @@ class Podcast(models.Model):
     video = models.FileField(upload_to='videos/', null=True, blank=True)
     thumbnail = models.FileField(upload_to='photos/', null=True, blank=True)
     category = models.CharField(max_length=255, verbose_name="category", blank=True)
+    views = models.BigIntegerField(blank=True)
+    earnings = models.BigIntegerField(blank=True)
 
     def __str__(self):
         return self.title
