684a6098964c1606ea0ea6c83ef4331fda126292
Arthur Lluisma <ajclluisma@gmail.com>
2025-04-11 04:03:04 +0800

    Add content to Channel Analytics under Podcast Studio

--- frontend/src/PodcastStudio/Analytics.jsx
+++ frontend/src/PodcastStudio/Analytics.jsx
@@ -3,7 +3,30 @@ import "./Analytics.css";
 export default function Analytics() {
   return (
     <main>
-      <p>Hello World</p>
+      <div className="main-container">
+        <div className="main-container__title">
+          <h1>Channel Analytics</h1>
+        </div>
+        <div className="main-container__data">
+          <div className="data__top">
+            <div className="data__followers">
+              <p>Followers</p>
+              <p>500.0K</p>
+            </div>
+            <div className="data__views">
+              <p>Views</p>
+              <p>1.0M</p>
+            </div>
+          </div>
+          <div className="data__bot">
+            <p>Earnings</p>
+            <p>PHP 1.0M</p>
+          </div>
+        </div>
+        <div className="top-podcasts">
+          <p>Top Podcasts</p>
+        </div>
+      </div>
     </main>
   );
 }
