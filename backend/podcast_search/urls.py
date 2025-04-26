ef02c2fcd9a78b576eee91ae80cd6914988e08d4
Waleed Lugod <lugodwaleed@gmail.com>
2025-04-26 18:38:42 +0800

    cleanup pdocast views

--- backend/podcast_search/urls.py
+++ backend/podcast_search/urls.py
@@ -5,13 +5,9 @@ from . import views
 
 
 router = DefaultRouter()
-# TODO: only use the route below
-router.register(r"", views.PodcastViewSet, basename="podcast")
-router.register(r"creators", views.CreatorPodcastViewSet, basename="creator")
+router.register(
+    r"",
+    views.PodcastViewSet,
+)
 
-urlpatterns = [
-    # BUG: this overwrote the other veiwset so the only permission is GET
-    # path("", views.PodcastSearch.as_view(), name="podcast-search-view"),
-    # path("<uuid:id>/", views.GetPodcast.as_view(), name="get_podcast"),
-    path("", include(router.urls)),
-]
+urlpatterns = router.urls
--- backend/podcast_search/views.py
+++ backend/podcast_search/views.py
@@ -1,52 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework import generics, filters, viewsets
-from rest_framework.decorators import action
-from rest_framework.exceptions import NotFound
-from rest_framework.parsers import JSONParser
+from rest_framework import filters, viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.authentication import SessionAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Podcast
 from .serializers import PodcastSerializer
-from user_management.models import CreatorModel
-from user_management.serializers import CreatorSerializer
 
 
-# TODO: remove all other apis and use this as the sole api
 class PodcastViewSet(viewsets.ModelViewSet):
     authentication_classes = [JWTAuthentication, SessionAuthentication]
-    permision_classes = [IsAuthenticated]
+    permision_classes = [IsAuthenticatedOrReadOnly]
     queryset = Podcast.objects.all()
     serializer_class = PodcastSerializer
 
-    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
     search_fields = ["title"]
-    filterset_fields = ["category"]
-
-
-class CreatorPodcastViewSet(viewsets.ModelViewSet):
-    authentication_classes = [JWTAuthentication]
-    permision_classes = [IsAuthenticated]
-    queryset = CreatorModel.objects.all()
-    serializer_class = CreatorSerializer
-
-    @action(detail=True, methods=["get", "post"])
-    def podcasts(self, request, pk=None):
-        try:
-            creator = self.get_object()
-        except CreatorModel.DoesNotExist:
-            raise NotFound("Creator not found")
-
-        if request.method == "GET":
-            podcasts = Podcast.objects.filter(creator=creator).order_by("-views")
-            serializer = PodcastSerializer(podcasts, many=True)
-            return JsonResponse(serializer.data, safe=False)
-
-        if request.method == "POST":
-            serializer = PodcastSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save(creator=creator)
-            return JsonResponse(serializer.data, status=201)
+    filterset_fields = ["category", "creator"]
+    ordering_fields = ["views"]
